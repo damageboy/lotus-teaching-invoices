9100a80423a74ff66d374f9dedbd590f241eb6c2
Dan Shechter <dans@houmus.org>
2026-03-08 15:28:36 +0100

    feat(gmail): add OAuth and Gmail API constants

---
+++ src/lib/gmail/constants.ts
@@ -0,0 +1,13 @@
+export const GOOGLE_CLIENT_ID =
+  '918178070743-m12oc3dv1rp40blkdomhc1767oigocpr.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_SECRET = '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';
+
+export const OAUTH_SCOPES = [
+  'https://www.googleapis.com/auth/gmail.compose',
+  'https://www.googleapis.com/auth/calendar.events',
+].join(' ');
+
+export const OAUTH_AUTH_URL = 'https://accounts.google.com/o/oauth2/auth';
+export const OAUTH_TOKEN_URL = 'https://oauth2.googleapis.com/token';
+export const GMAIL_API_BASE = 'https://gmail.googleapis.com/gmail/v1';
+export const TOKEN_FILE = 'gmail-tokens.json';
