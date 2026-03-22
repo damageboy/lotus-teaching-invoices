79eae4fa63ade4aa622ce94380bb9befed1979b0
Dan Shechter <dans@houmus.org>
2026-03-22 13:50:01 +0100

    feat: add Calendar API base URL constant

--- src/lib/gmail/constants.ts
+++ src/lib/gmail/constants.ts
@@ -10,4 +10,5 @@ export const OAUTH_SCOPES = [
 export const OAUTH_AUTH_URL = 'https://accounts.google.com/o/oauth2/auth';
 export const OAUTH_TOKEN_URL = 'https://oauth2.googleapis.com/token';
 export const GMAIL_API_BASE = 'https://gmail.googleapis.com/gmail/v1';
+export const CALENDAR_API_BASE = 'https://www.googleapis.com/calendar/v3';
 export const TOKEN_FILE = 'gmail-tokens.json';
